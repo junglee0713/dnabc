d702f3afdcf5113b4850f62b52dba6788f528cd3
Kyle Bittinger <kylebittinger@gmail.com>
2015-06-04 15:08:55 -0400

    Removed unused Run class.

--- test/test_models.py
+++ test/test_models.py
@@ -15,17 +15,5 @@ class SampleTests(unittest.TestCase):
         self.assertEqual(s.barcode, "AGCT")
 
 
-class RunTests(unittest.TestCase):
-    def test_file_format(self):
-        def R(fp):
-            return Run(123, None, None, None, None, fp, None)
-        self.assertEqual(R("file/").file_format, "FASTQ")
-        self.assertEqual(R("file/").file_format, "FASTQ")
-        self.assertEqual(R("file.fastq.gz").file_format, "FASTQ")
-        self.assertEqual(R("file.fna").file_format, "FASTA")
-        self.assertEqual(R("file.fasta").file_format, "FASTA")
-        self.assertEqual(R("file.sff").file_format, "SFF")
-
-
 if __name__ == "__main__":
     unittest.main()
